9fbd95302a6e3d5ebdc6d749447c3c8f6d717c91
glomen <glomen@glomen.ru>
2023-08-03 02:08:15 +0300

    create tmpl, view array

--- app/controller/pages.go
+++ app/controller/pages.go
@@ -1,13 +1,25 @@
 package controller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	text := "Приветствую тебя на стартовой странице этого сайта!"
-	fmt.Fprint(rw, text)
+	path := filepath.Join("public", "html", "startStaticPage.html")
+	// создаем html шаблон
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	// выводим шаблон в браузер при запросе
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 }
--- app/controller/users.go
+++ app/controller/users.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"encoding/json"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/Glomen/goblog/app/model"
 	"github.com/julienschmidt/httprouter"
@@ -15,8 +16,16 @@ func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-	//возвращаем список клиенту в формате JSON
-	err = json.NewEncoder(rw).Encode(users)
+	//указываем путь к файлу с шаблоном
+	main := filepath.Join("public", "html", "usersDynamicPage.html")
+	// создаем html шаблон
+	tmpl, err := template.ParseFiles(main)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	//исполняем именованный шаблон "users", передавая туда массив со списком пользователей
+	err = tmpl.ExecuteTemplate(rw, "users", users)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
