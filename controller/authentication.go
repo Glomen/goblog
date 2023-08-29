95959c2a957c86e0ae2a76bd1a1296ef6d03bc7c
glomen <glomen@glomen.ru>
2023-08-30 01:57:52 +0300

    add register user

--- controller/authentication.go
+++ controller/authentication.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 
+	"github.com/Glomen/goblog/helper"
 	"github.com/Glomen/goblog/model"
 	"github.com/gin-gonic/gin"
 )
@@ -27,4 +28,33 @@ func Register(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"user": saveUser})
+}
+
+func Login(context *gin.Context) {
+
+	var input model.AuthenticationInput
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := model.FindUserByUsername(input.Username)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = user.ValidatePassword(input.Password)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	jwt, err := helper.GenerateJWT(user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"jwt": jwt})
 }
